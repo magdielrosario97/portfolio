615269848cd65e17014f22cf9f56e32aff48b28e
Magdiel Rosario <msrdev.tech@gmail.com>
2025-05-06 03:12:00 +0000

    Add Web Analytics

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,5 @@
+import { Analytics } from "@vercel/analytics/react";
+
 import Hero from "./components/Hero";
 import Projects from "./components/Projects";
 import About from "./components/About";
@@ -12,6 +14,7 @@ function App() {
          <Skills />
          <Projects />
          <Contact />
+         <Analytics />
       </main>
    );
 }
